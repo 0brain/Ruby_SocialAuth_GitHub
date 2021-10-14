09e385f2c73e5ec6f26aecc5b054fd2f79a5332a
nazar <nazar.khort@gmail.com>
2021-10-14 14:50:34 +0300

    Added good_resp variable,which allows to ask in a template if such a user exists and display a message 'Not found' if not.

--- users/views.py
+++ users/views.py
@@ -9,9 +9,11 @@ def get_from_github(request):
         username = request.GET['username']
         url1 = f'https://api.github.com/users/{username}'
         url2 = f'https://api.github.com/users/{username}/repos'
-        response1 = requests.get(url1)
-        response2 = requests.get(url2)
-        user = response1.json()
-        repo_list = response2.json()
+        response_username = requests.get(url1)
+        good_resp = (response_username.status_code == 200)
+        response_username_repos = requests.get(url2)
+        user = response_username.json()
+        user['exist'] = good_resp
+        repo_list = response_username_repos.json()
     return render(request, 'index.html', {'user': user, 'repo_list': repo_list})
 
